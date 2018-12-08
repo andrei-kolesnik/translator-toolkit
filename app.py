0451e78a103364061029ade9b8c9b997bbcbd55a
andrei-kolesnik <andrei.kolesnik@gmail.com>
2018-12-08 14:49:01 -0800

    sidebar, navigation, collocations

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 # Imports
 import re
+from os import listdir
+from os.path import isfile, join 
+import sys
+from io import StringIO
+from flask import request
+from werkzeug.utils import secure_filename
 import scipy
 import numpy as np
 import pandas as pd
 import matplotlib as pl
 import nltk
 from nltk import FreqDist
-nltk.download('punkt')
+#nltk.download('punkt')
 from nltk import word_tokenize,sent_tokenize
+from nltk.corpus import PlaintextCorpusReader
 
 from flask import (
     Flask,
@@ -19,23 +26,20 @@ from flask import (
 # Flask  Setup and Routes
 app = Flask(__name__)
 
-# Prepare the document
-from nltk.corpus import PlaintextCorpusReader
-corpus_loc = './data/'
-docs = nltk.corpus.PlaintextCorpusReader(corpus_loc, '.*\.txt')
-docs_processed = nltk.Text(docs.words())
-sents = []
-cnt = 1
-for sent in docs.sents():
-    sents.append({"index": cnt, "sent": ' '.join(sent)})
-    cnt = cnt+1
+################################################################################
+# Globs
+################################################################################
+Globs = {
+    'file': '',
+    'dir': './data/'
+}
+global docs, docs_processed, sents
 
 excl = ['a', 'after', 'all', 'also', 'an', 'and', 'another', 'any', 'are', 'as', 'at', 'away', 'be', 'been', 'beforehand', 'being', 'both', 'but', 'by', 'can', 'cannot', 'do', 'does', 'each', 'for', 'from', 'further', 'furthermore', 'has', 'have', 'having', 'here', 'hereby', 'hereinabove', 'hereinbefore', 'heretofore', 'how', 'if', 'in', 'into', 'is', 'it', 'its', 'may', 'more', 'no', 'non', 'nor', 'not', 'of', 'off', 'on', 'only', 'or', 'other', 'out', 'own', 'same', 'since', 'so', 'such', 'than', 'that', 'the', 'their', 'then', 'there', 'therebetween', 'thereby', 'therein', 'thereof', 'thereto', 'they', 'this', 'those', 'thus', 'to', 'too', 'up', 'via', 'was', 'what', 'when', 'where', 'wherein', 'whether', 'which', 'whose', 'with', 'within']
 
-import sys
-from io import StringIO
-
-# Class to capture output of functions with no return
+################################################################################
+# Utils
+################################################################################
 class capt_stdout:
     def __init__(self):
         self._stdout = None
@@ -53,13 +57,101 @@ class capt_stdout:
     def string(self):
         return self._string_io.getvalue()
 
-# Create route that renders index.html template
+
+def check():
+    if len(Globs['file']) == 0:
+        for filename in listdir(Globs['dir']):
+            fullname = join(Globs['dir'], filename)
+            if isfile(fullname):
+                Globs['file'] = filename ##// redo to use the last used
+                openfile(filename)
+
+
+def openfile(filename):
+    global docs, docs_processed
+    docs = nltk.corpus.PlaintextCorpusReader(Globs['dir'], Globs['file'])
+    docs_processed = nltk.Text(docs.words())
+
+################################################################################
+# Routs
+################################################################################
 @app.route("/")
 def index():
-    return render_template("index.html", sents = sents)
+    return files()
 
+
+################################################################################
+@app.route("/files")
+def files():
+    global Globs
+    files = []
+    for filename in listdir(Globs['dir']):
+        fullname = join(Globs['dir'], filename)
+        if isfile(fullname):
+            f = open(fullname)
+            files.append({
+                "name": filename,
+                "first_line": f.readline()
+            })
+            f.close()
+    return render_template("files.html", files = files, globs = Globs)
+
+
+################################################################################
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    global Globs
+    if request.method == 'POST':
+        f = request.files['new_file']
+        f.save(Globs['dir'] + secure_filename(f.filename))
+        Globs['file'] = f.filename
+    return files()
+
+
+################################################################################
+@app.route("/f/<name>")
+def show_file(name):
+    global docs, docs_processed
+    Globs['file'] = name
+    openfile(name)
+    return sents1()
+
+
+################################################################################
+@app.route("/s1")
+@app.route("/s1/<word>")
+def sents1(word = ''):
+    global docs, sents
+    check()
+    sents = []
+    cnt = 1
+    for sent in docs.raw().split("\n"):
+        sent = sent.strip()
+        if len(sent) > 0 and not sent.isspace():
+            if len(word) == 0 or sent.find(word) > -1:
+                sents.append({"index": cnt, "sent": sent})
+            cnt = cnt+1
+    return render_template("sents.html", globs = Globs, sents = sents)
+
+
+################################################################################
+@app.route("/s2")
+def sents2():
+    global docs, sents
+    check()
+    sents = []
+    cnt = 1
+    for sent in docs.sents():
+        sents.append({"index": cnt, "sent": ' '.join(sent)})
+        cnt = cnt+1
+    return render_template("sents.html", globs = Globs, sents = sents)
+
+
+################################################################################
 @app.route('/freq')
 def freq(): 
+    global docs, docs_processed
+    check()
     fdist = FreqDist()
     for word in docs.words():
         word = word.lower()
@@ -69,9 +161,14 @@ def freq():
     freq_sorted = sorted(fdist.items(), key=lambda item: (item[1], item[0]), reverse=True)
     return jsonify(freq_sorted[:50])
 
+
+################################################################################
+@app.route('/concordance/<word>')
 @app.route('/concordance/<word>/<int:count>')
 def concordance(word, count = 25): 
-    con_list = docs_processed.concordance_list(word, width = 80, lines = count)   
+    global docs, docs_processed
+    check()
+    con_list = docs_processed.concordance_list(word, width=80, lines=count)
     con_data = []
     for c in con_list:
         con_data.append({
@@ -80,8 +177,27 @@ def concordance(word, count = 25):
             "right_print": c.right_print})
     return jsonify(con_data)
 
+
+################################################################################
+@app.route('/collocations')
+@app.route('/collocations/<int:count>')
+def collocations(count = 25): 
+    global docs, docs_processed
+    check()
+    with capt_stdout() as out:
+        docs_processed.collocations(num = count)
+        collocations_output = out.string
+    col_data = []
+    for c in collocations_output.split(';'):
+        col_data.append({"term": c})
+    return jsonify(col_data)
+
+
+################################################################################
 @app.route('/contexts/<word>')
 def contexts(word): 
+    global docs, docs_processed
+    check()
     with capt_stdout() as out:
         docs_processed.common_contexts([word])
         common_contexts_output = out.string
@@ -93,8 +209,11 @@ def contexts(word):
                 "left_print": c[:ind], 
                 "query": word,
                 "right_print": c[ind+1:]})
-
     return jsonify(con_data)
 
+
+################################################################################
+# Main
+################################################################################
 if __name__ == '__main__':
     app.run(debug=True)
--- static/actions.js
+++ static/actions.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        // open or close navbar
+        $('#sidebar, #content').toggleClass('active');
+        // close dropdowns
+        $('.collapse.in').toggleClass('in');
+        // and also adjust aria-expanded attributes we use for the open/closed arrows
+        // in our CSS
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
     $("#freq").click(function() {
         $.getJSON("/freq", function(result) {
             var tbl = document.createElement('table');
@@ -9,7 +24,8 @@ $(document).ready(function() {
                 var td2 = tr.insertCell(); 
                 td1.setAttribute('class', "left");
                 td2.setAttribute('class', "right");
-                td1.innerHTML = "<a href=\"#\" class=\"word\" onclick=\"concordance('"+field[0]+"', "+field[1]+')">'+field[0]+'</a>';
+//                td1.innerHTML = "<a href=\"#\" class=\"word\" onclick=\"concordance('"+field[0]+"', "+field[1]+')">'+field[0]+'</a>';
+                td1.innerHTML = '<a href="/s1/'+field[0]+'"' + " class=\"word\" onclick=\"concordance('"+field[0]+"', "+field[1]+')">'+field[0]+'</a>';
                 td2.appendChild(document.createTextNode(field[1]));
             });
             var div = document.getElementById('left1');
@@ -17,6 +33,23 @@ $(document).ready(function() {
             div.appendChild(tbl);
         });
     });
+
+    $("#collocations").click(function () {
+        $.getJSON("/collocations", function (result) {
+            var tbl = document.createElement('table');
+            tbl.setAttribute('class', "basic");
+            $.each(result, function (i, field) {
+                var tr = tbl.insertRow();
+                var td1 = tr.insertCell();
+                td1.setAttribute('class', "left");
+                td1.appendChild(document.createTextNode(field.term));
+            });
+            var div = document.getElementById('left1');
+            div.innerHTML = '<b>Collocations:</b>';
+            div.appendChild(tbl);
+        });
+    });
+
 /*
     $("body").on("click", ".word", function(e) {
         var word = $(e.target).text();
@@ -59,10 +92,11 @@ function concordance(word, count) {
             td2.appendChild(document.createTextNode(field.query));
             td3.appendChild(document.createTextNode(field.right_print));
         });
-        var div = document.getElementById('right1');
+        var div = document.getElementById('right2');
         div.innerHTML = '<b>All Concordance:</b>';
         div.appendChild(tbl);
     });
+    /*
     $.getJSON("/concordance/"+word+"/"+count, function(result) {
         var tbl = document.createElement('table');
         tbl.setAttribute('class', "basic");
@@ -82,4 +116,7 @@ function concordance(word, count) {
         div.innerHTML = '<b>Unique Contexts:</b>';
         div.appendChild(tbl);
     });
+    */
 }
+
+
